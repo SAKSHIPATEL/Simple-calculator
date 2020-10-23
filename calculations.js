2103f9df45c747983b92687e6363649a15871c57
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-23 08:44:00 +0530

    Code of designing of calculator using JS

---
+++ calculations.js
@@ -0,0 +1,15 @@
+function insert(num){
+    document.form.textview.value=document.form.textview.value+ num;
+
+}
+
+function equal(){
+    exp=document.form.textview.value;
+    if(exp){
+        document.form.textview.value=eval(exp);
+    }
+}
+
+function escape(){
+    document.form.textview.value="";
+}
